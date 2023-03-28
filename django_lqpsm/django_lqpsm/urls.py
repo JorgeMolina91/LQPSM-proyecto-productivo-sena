ed015058d998bb34daf96e159494e59589adc05a
Jorge Molina <jorgemolina2097@gmail.com>
2023-03-27 21:40:38 -0500

    Preparando el entorno de desarrollo

---
+++ django_lqpsm/django_lqpsm/urls.py
@@ -0,0 +1,37 @@
+"""django_lqpsm URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="APIDocs Lo Que Pida SU-MERCED", # Titulo de mi proyecto
+      default_version='v1',
+      description="Documentación de la herramienta tecnológica del restaurante 'Lo que pida SU-MERCED'", # Descripción de mi proyecto
+      terms_of_service="",  #Una pagina donde tenga mis terminos y condiciones
+      contact=openapi.Contact(email="admin@admin.com"),  #Mi email de contacto
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
